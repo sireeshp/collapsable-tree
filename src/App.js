633299e22a65b23828a22da6ed29a9b7a643ed40
wholelot <sireesh.pangaluri@hotmail.co.uk>
2021-10-27 09:21:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
-
+import data from './data.json';
+import TreeView from './Chart'
 function App() {
+  const treeRef = useRef(null);
+  useEffect(() => {
+    if (treeRef.current) {
+      TreeView(treeRef.current, data);
+    }
+    return () => {
+      treeRef.current = null;
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={treeRef}>
+
+      </div>
     </div>
   );
 }
